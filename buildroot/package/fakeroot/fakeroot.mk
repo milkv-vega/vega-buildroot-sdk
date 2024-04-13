39139396f1457d7265710705d573837549dc00a4
Michael Fuckner <michael@fuckner.net>
2024-04-13 07:03:23 +0200

    Update fakeroot to 1.34 and as a dependency autoconf 2.72

--- buildroot/package/fakeroot/fakeroot.mk
+++ buildroot/package/fakeroot/fakeroot.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-FAKEROOT_VERSION = 1.20.2
-FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.bz2
-FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20141005T221953Z/pool/main/f/fakeroot
+FAKEROOT_VERSION = 1.34
+FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.gz
+FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20240401T084438Z/pool/main/f/fakeroot
 
 HOST_FAKEROOT_DEPENDENCIES = host-acl
 # Force capabilities detection off
